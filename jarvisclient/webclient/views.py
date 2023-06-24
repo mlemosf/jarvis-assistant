1b17b641651a24ec5b6d4a5101aa56cf529cffb9
mlemosf <matheuslemosf@protonmail.com>
2023-06-24 19:41:33 +0000

    Código para gravação e envio de áudio pro backend

--- jarvisclient/webclient/views.py
+++ jarvisclient/webclient/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, JsonResponse
 
 # Create your views here.
 
 class VoiceRecordingView(TemplateView):
     template_name = "webclient/recorder.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        print(request.FILES)
+        return JsonResponse({
+            "message": "testmessage"
+        })
+
+class ResponseView(TemplateView):
+    template_name = "webclient/response.html"
 
