085076756afb07a6106a63b52f0b2e871d474a3b
mlemosf <matheuslemosf@protonmail.com>
2023-08-07 13:45:52 +0000

    Uso do django-environ
    Arquivo de variáveis de ambiente.

--- jarvisclient/config/settings.py
+++ jarvisclient/config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wp24ikrjy%e_pp9q+57z!b=8jxph&fgg6(jk^aj1-5_zp06y%r'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Variáveis de ambiente
-JARVIS_ENDPOINT="http://localhost:1880/jarvis"
+JARVIS_ENDPOINT= env('JARVIS_ENDPOINT')
 
 # Application definition
 
